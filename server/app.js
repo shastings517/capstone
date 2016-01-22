e6d28c8cea905d0a44e2fe011aaf9bb0943ae0fe
Samuel Hastings <shastings517@gmail.com>
2016-01-21 22:53:52 -0800

    heroku setup

--- server/app.js
+++ server/app.js
@@ -131,6 +131,6 @@ io.on('connection', function(socket) {
 
 });
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 console.log('Server listening on port 3000');
 
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/satauth");
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/satauth");
 mongoose.set("debug",true);
 
 module.exports.User = require("./user");
